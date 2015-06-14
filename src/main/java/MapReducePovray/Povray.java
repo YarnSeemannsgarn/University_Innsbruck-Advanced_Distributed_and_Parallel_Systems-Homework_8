06fa258141484c7680c34868dd708656567b26b5
YarnSeemannsgarn <janschlenker1990@gmail.com>
2015-06-14 13:34:33 +0200

    Add Description for Mapper and Main

--- src/main/java/MapReducePovray/Povray.java
+++ src/main/java/MapReducePovray/Povray.java
@@ -7,10 +7,24 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+/*
+ * Runs MapReduce Povray workflow on Hadoop
+ * 
+ * @author Jan Schlenker
+ * 
+ */
 public class Povray {
+	public static String INVALID_SYNTAX = "Invalid number of parameters.\n"
+			+ "Usage: $HADDOP_HOME/bin/hadoop jar target/map-reduce-povray-0.0.1-SNAPSHOT.jar MapReducePovray.Povray <input-dir> <output-dir>";
+	
 	public static void main(String[] args) throws Exception {
+		if(args.length != 2) {
+			System.err.println(INVALID_SYNTAX);
+			System.exit(1);
+		}
+		
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "word count");
+		Job job = Job.getInstance(conf, "povray");
 		job.setJarByClass(Povray.class);
 		job.setMapperClass(PovrayMapper.class);
 		job.setCombinerClass(PovrayReducer.class);
--- src/main/java/MapReducePovray/PovrayMapper.java
+++ src/main/java/MapReducePovray/PovrayMapper.java
@@ -8,8 +8,20 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
-// Input format <?, Text>, where Text only contains the number of the frame
-// Output format <Intwritable, BytesWritable>, where IntWritable is always 1 and BytesWritable contains the generated picture
+/**
+ * Mapper implementation to render pictures using Povray.
+ * 
+ * Input format is <?, Text>, where 
+ * Text lines contain the number of frames, the subset start and the subset end frame
+ * 
+ * Output format is <Intwritable, FramesWritable>, where 
+ * IntWritable is always 1 to combine all frames in the Reducer and 
+ * BytesWritable contains the generated picture
+ * 
+ * @author Jan Schlenker
+ *
+ */
+
 public class PovrayMapper extends Mapper<Object, Text, IntWritable, FrameWriteable>{
 
 	private final static IntWritable one = new IntWritable(1);
