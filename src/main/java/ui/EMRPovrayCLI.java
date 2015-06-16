d2e7d00ba85a9ef24262496880bde69d7a4ba59e
Sebastian Sams <sebastian.sams@student.uibk.ac.at>
2015-06-16 11:59:22 +0200

    frontend: wait for job completion, improve error handling

--- src/main/java/ui/EMRPovrayCLI.java
+++ src/main/java/ui/EMRPovrayCLI.java
@@ -14,17 +14,24 @@ import com.amazonaws.auth.PropertiesCredentials;
 public final class EMRPovrayCLI {
 
 	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("usage: EMRPovrayCLI clusterId frameCount");
+			System.exit(1);
+		}
+		final String clusterId = args[0];
+		final int frames = Integer.parseInt(args[1]);
+		
 		AWSCredentials credentials = null;
 		try {
 		    credentials = new PropertiesCredentials(new File("AwsCredentials.properties"));
 		} catch (IOException e1) {
 		    System.out.println("Credentials were not properly entered into AwsCredentials.properties.");
 		    System.out.println(e1.getMessage());
-		    System.exit(1);
+		    System.exit(2);
 		}
 		
 		try {
-			new PovrayRunner(credentials, "", "povray-emr").render(12, new File("out.gif"));
+			new PovrayRunner(credentials, clusterId, "povray-emr").render(frames, new File("out.gif"));
 		} catch (IOException e) {
 			System.out.println("rendering error");
 			e.printStackTrace();
--- src/main/java/ui/PovrayRunner.java
+++ src/main/java/ui/PovrayRunner.java
@@ -5,15 +5,22 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import com.amazonaws.services.elasticmapreduce.model.ActionOnFailure;
 import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsRequest;
+import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsResult;
+import com.amazonaws.services.elasticmapreduce.model.DescribeStepRequest;
 import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
+import com.amazonaws.services.elasticmapreduce.model.Step;
 import com.amazonaws.services.elasticmapreduce.model.StepConfig;
+import com.amazonaws.services.elasticmapreduce.model.StepState;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.DeleteObjectsRequest;
@@ -71,35 +78,79 @@ public class PovrayRunner {
 	 * Render the animation.
 	 * @param frames number of frames to render
 	 * @param output local file for storing the generated animation
-	 * @throws IOException if an I/O error occurs
+	 * @throws IOException if an error occurs
 	 */
 	public void render(int frames, File output) throws IOException {
-		// cleanup old files
-		deleteS3directory("input");
-		deleteS3directory("output");
-		
-		// prepare input files for the job
-		prepareInput(frames);
-		
-		// create and run the job
-		final String s3BaseUrl = "s3://" + mStorageBucket + "/";
-		HadoopJarStepConfig jarStepConfig = new HadoopJarStepConfig()
-	        .withJar(s3BaseUrl + JAR_NAME)
-	        .withMainClass(JAR_MAIN_CLASS)
-	        .withArgs(s3BaseUrl + "input/", s3BaseUrl + "output/");
-		mMapReduceClient.addJobFlowSteps(new AddJobFlowStepsRequest(mClusterId).withSteps(new StepConfig("render", jarStepConfig)));
-		
-		// download the generated gif
-		downloadResult(output);
+		try {
+			// cleanup old files
+			deleteS3directory("input");
+			deleteS3directory("output");
+			
+			// prepare input files for the job
+			prepareInput(frames);
+			
+			// create and run the job
+			final String s3BaseUrl = "s3://" + mStorageBucket + "/";
+			HadoopJarStepConfig jarStepConfig = new HadoopJarStepConfig()
+			    .withJar(s3BaseUrl + JAR_NAME)
+			    .withMainClass(JAR_MAIN_CLASS)
+			    .withArgs(s3BaseUrl + "input/", s3BaseUrl + "output/");
+			final AddJobFlowStepsResult stepResult = mMapReduceClient
+					.addJobFlowSteps(new AddJobFlowStepsRequest(mClusterId)
+							.withSteps(new StepConfig("render", jarStepConfig)
+							.withActionOnFailure(ActionOnFailure.CONTINUE)));
+			
+			if (waitForJobCompletion(stepResult.getStepIds().get(0), 2, TimeUnit.SECONDS)) {	
+				// download the generated gif
+				downloadResult(output);
+			} else {
+				throw new IOException("job execution failed");
+			}
+		} catch (AmazonClientException e) {
+			throw new IOException("error when communicating with Amazon AWS", e);
+		}
 	}
 	
+	/**
+	 * Wait until the job execution is completed.
+	 * @param stepId id of the step
+	 * @param checkInterval time to wait between periodic checks for completion
+	 * @param unit time unit of the specified interval
+	 * @return <code>true</code> if the job has been completed successfully, <code>false</code> if it failed
+	 */
+	private boolean waitForJobCompletion(String stepId, int checkInterval, TimeUnit unit) {
+		try {
+			while (true) {
+				final Step step = mMapReduceClient.describeStep(new DescribeStepRequest().withClusterId(mClusterId).withStepId(stepId)).getStep();
+				switch (StepState.fromValue(step.getStatus().getState())) {
+				case COMPLETED:
+					return true;
+					
+				case CANCELLED:
+				case FAILED:
+				case INTERRUPTED:
+					return false;
+					
+				case PENDING:
+				case RUNNING:
+				default:
+					Thread.sleep(unit.toMillis(checkInterval));
+					break;
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
 	/**
 	 * Delete a "directory" in S3. A directory is equivalent to all files starting with a common prefix and are delimited by a slash.
 	 * @param directory the directory to delete (without the slash at the end)
 	 */
 	private void deleteS3directory(String directory) {
+		// delete contents
 		final List<S3ObjectSummary> objects = mS3Client.listObjects(mStorageBucket, directory + "/").getObjectSummaries();
-		
 		if (objects.size() > 0) {
 			final List<String> keys = new ArrayList<String>(objects.size());
 			for (final S3ObjectSummary object : objects) {
@@ -107,6 +158,9 @@ public class PovrayRunner {
 			}
 			mS3Client.deleteObjects(new DeleteObjectsRequest(mStorageBucket).withKeys(keys.toArray(new String[0])));
 		}
+		
+		// delete directory itself
+		mS3Client.deleteObject(mStorageBucket, directory);
 	}
 
 	/**
